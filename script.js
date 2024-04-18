739a99dffaa781cf44aa07d2d5c925d31c455a11
rlyweirdname <rlyweirdname@gmail.com>
2024-04-18 13:52:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const ctaLink = document.querySelector(".shop");
+
+ctaLink.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const targetSection = document.querySelector("#shop");
+  targetSection.scrollIntoView({ behavior: "smooth" });
+});
